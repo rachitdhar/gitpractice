b653f7067e210f3ae25af4278fc3495132e8e08e
rachitdhar <75770595+rachitdhar@users.noreply.github.com>
2021-01-10 14:04:31 +0530

    Update gitpush-test.py

--- gitpush-test.py
+++ gitpush-test.py
@@ -5,5 +5,13 @@ for i in range(5):
 # To check if we can view the changes made when someone else has edited ur file
 def testfunc():
     print("Let\'s gooo")
+
+# ok, so I found that when someone else directly makes a change and commits it to the main branch, then it won't highlight that change in vscode
+# it will only show the updated file directly which can be viewed when u press synchronize button (on bottom-left) in vscode.
+
+# let's try using a pull request option and see what happens in that case
+
+def pulltest():
+    print("Hello world pt2")
     
 # just a test file to see if git commit and git push works properly

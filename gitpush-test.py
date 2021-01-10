a480bc4d478c8e1f814bd4f9223259c112b0cd0a
rachitdhar <75770595+rachitdhar@users.noreply.github.com>
2021-01-10 13:59:27 +0530

    Update gitpush-test.py

--- gitpush-test.py
+++ gitpush-test.py
@@ -2,4 +2,8 @@ for i in range(5):
     print("Hello world")
     print("adding a new line now")
 
+# To check if we can view the changes made when someone else has edited ur file
+def testfunc():
+    print("Let\'s gooo")
+    
 # just a test file to see if git commit and git push works properly

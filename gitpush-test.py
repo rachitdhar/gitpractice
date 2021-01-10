c97b0ef8dab05bfef69f64c7142e74785e827ce8
rachitdhar <75770595+rachitdhar@users.noreply.github.com>
2021-01-10 14:34:12 +0530

    testing how branches work

--- gitpush-test.py
+++ gitpush-test.py
@@ -13,5 +13,24 @@ def testfunc():
 
 def pulltest():
     print("Hello world pt2")
-    
+
+# ok great, so now whenever someone makes a change in a file that someone else is working on, then
+# it is better to make a pull request rather than directly add it to the main branch, bcs that might
+# make it difficult for the other person in case he has already made his own changes without including
+# the other guy's code yet.
+
+# let's see if we can make a pull request from vscode
+
+for _ in range(len(str(12334))):
+    testfunc()
+    pulltest()
+
+    print("Hello world! (pt3)")
+
+# ok so what we have to do is, click the "main" written at the bottom-left of vscode. This tells what
+# the branch is within which we are working.
+# on clicking it, u can choose "create a new branch from" option. Enter the new name. (eg: rachitdhar-patch-2)
+# and then select the branch name from which u have created this new branch. (In this case, that is the "main" branch)
+# then just do a commit and push normally as we did before
+
 # just a test file to see if git commit and git push works properly
